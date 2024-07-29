02722356c85ca0cf53637d85004d04c6650b8d18
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-07-30 00:39:42 +1000

    Extract code into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { getMovieById } from './services/TMDBService.js';
-import selectedMovies from './data/featuredMovies.json';
+import { HomePage } from './pages/HomePage/HomePage';
 
-function App() {
-  const [featuredMovies, setFeaturedMovies] = useState<TMDBMovie[] | null>(
-    null
-  );
-
-  useEffect(() => {
-    const fetchFeaturedMovies = async () => {
-      const selectedMoviesPromises = selectedMovies.map((selectedMovie) =>
-        getMovieById(selectedMovie.id)
-      );
-      return Promise.all(selectedMoviesPromises);
-    };
-    fetchFeaturedMovies().then((res) => setFeaturedMovies(res));
-  });
-
+export const App = () => {
   return (
     <>
-      <h1>Hello world!</h1>
-      <button onClick={() => console.log(featuredMovies)}>
-        Get movie details
-      </button>
+      <HomePage />
     </>
   );
-}
+};
 
 export default App;
