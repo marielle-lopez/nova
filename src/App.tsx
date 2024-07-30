e0753d665a17a4406c921b2720a532808efd2b0b
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-07-30 22:41:37 +1000

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar/NavBar';
 import { HomePage } from './pages/HomePage/HomePage';
+import { MoviesPage } from './pages/MoviesPage/MoviesPage';
 
 export const App = () => {
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="*" element={<h1>Oh... this page doesn't exist!</h1>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
